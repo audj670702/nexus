3fa079b95d67d954d771d05641852bf614904bb7
SCaD <gestor.isp@gmail.com>
2026-09-23 09:44:53 -0600

    NEXUS v0.2.39: cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="nexus-v0.2.38";
+const CACHE_NAME="nexus-v0.2.39";
 const APP_SHELL=["./","./index.html","./css/nexus.css","./js/app.js","./js/context.js","./js/auth.js","./js/navigation.js","./js/modules.js","./js/tv.js","./js/mns.js","./mns-frontend-v052.html","./mns-frontend.html","./manifest.json"];
 
 self.addEventListener("install",event=>{
