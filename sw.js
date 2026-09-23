a99b1861beae7d55daaa308226edc3be717f5a46
SCaD <gestor.isp@gmail.com>
2026-09-23 10:36:41 -0600

    NEXUS v0.2.42 estabiliza altura modal Programación

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="nexus-v0.2.41";
+const CACHE_NAME="nexus-v0.2.42";
 const APP_SHELL=["./","./index.html","./css/nexus.css","./js/app.js","./js/context.js","./js/auth.js","./js/navigation.js","./js/modules.js","./js/tv.js","./js/mns.js","./js/documents.js","./js/schedule.js","./mns-frontend-v052.html","./mns-frontend.html","./manifest.json"];
 
 self.addEventListener("install",event=>{
