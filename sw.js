02b5155d7f6c6bd9aaa8ec2b037ea9ddd26d6ad8
SCaD <gestor.isp@gmail.com>
2026-09-23 10:32:24 -0600

    NEXUS v0.2.41 habilita módulo Programación

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="nexus-v0.2.40";
-const APP_SHELL=["./","./index.html","./css/nexus.css","./js/app.js","./js/context.js","./js/auth.js","./js/navigation.js","./js/modules.js","./js/tv.js","./js/mns.js","./js/documents.js","./mns-frontend-v052.html","./mns-frontend.html","./manifest.json"];
+const CACHE_NAME="nexus-v0.2.41";
+const APP_SHELL=["./","./index.html","./css/nexus.css","./js/app.js","./js/context.js","./js/auth.js","./js/navigation.js","./js/modules.js","./js/tv.js","./js/mns.js","./js/documents.js","./js/schedule.js","./mns-frontend-v052.html","./mns-frontend.html","./manifest.json"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
